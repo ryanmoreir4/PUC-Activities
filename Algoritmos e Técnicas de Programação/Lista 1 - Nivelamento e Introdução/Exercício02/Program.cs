e6451fdf09f56ce333cdc7db626c8480f7ac8fcb
Ryan Moreira <rmpoliveira@sga.pucminas.br>
2024-10-24 16:00:18 -0300

    adicionando exercicio 02

---
+++ Algoritmos e Técnicas de Programação/Lista 1 - Nivelamento e Introdução/Exercício02/Program.cs
@@ -0,0 +1,33 @@
+﻿/* Escreva um programa que leia a base e a altura de um retângulo
+e imprima o seu perímetro, área e diagonal.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double perimetro, area, diagonal;
+
+        Console.Write("Insira o valor referente a base do triângulo: ");
+        double baseTriangulo = double.Parse(Console.ReadLine());
+
+        Console.Write("Insira o valor referente a altura do triângulo: ");
+        double alturaTriangulo = double.Parse(Console.ReadLine());
+
+        perimetro = 2 * (baseTriangulo + alturaTriangulo);
+        area = baseTriangulo * alturaTriangulo;
+        diagonal = Math.Sqrt(Math.Pow(baseTriangulo, 2) + Math.Pow(alturaTriangulo, 2));
+
+        Console.WriteLine($"O perímetro do triângulo é: {perimetro}");
+        Console.WriteLine($"A área do triângulo é: {area}");
+        Console.WriteLine($"A diagonal do triângulo é: {diagonal}");
+
+    }
+}
